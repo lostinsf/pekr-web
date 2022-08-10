f27aacbe1934a15276bfb305655461e358963404
이봉주 <ibongju@ibongjuui-MacBookPro.local>
2022-08-10 18:56:55 +0900

    [Enhancements] issue/5 - next-seo 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '@pages/globals.css';
 import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import config from '@utils/next-seo/next-seo.json';
+import { createEmotionCache } from '@utils/emotion/emotion';
+import { DefaultSeo } from 'next-seo';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type MyAppProps = {
+  emotionCache?: EmotionCache;
+} & AppProps;
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = createEmotionCache(), pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <DefaultSeo {...config} />
+      <Component {...pageProps} />;
+    </CacheProvider>
+  );
 }
 
 export default MyApp;
